dea32b6671d4c6b4e9081e7932dd08d92e4358db
olabamipe.taiwo <olabamipe.taiwo@mypatricia.co>
2023-02-08 02:58:02 +0100

    dev::added reusable button component

---
+++ src/components/Button.vue
@@ -0,0 +1,70 @@
+<template>
+  <component
+    class="btn font-medium flex items-center justify-center cursor-pointer border focus:outline-none"
+    :is="buttonType"
+    :href="href"
+    :type="type"
+    :class="[btnClasses, customClass]"
+    v-bind="$attrs"
+  >
+    <slot></slot>
+  </component>
+</template>
+<script>
+export default {
+  props: {
+    href: {
+      required: false,
+      type: String,
+      default: null,
+    },
+    type: {
+      type: String,
+      default: "button",
+    },
+    color: {
+      type: String,
+      default: "teal",
+    },
+    size: {
+      type: String,
+      default: "md", //sm, md, lg
+    },
+    customClass: {
+      required: false,
+      type: String,
+      default: "",
+    },
+    outline: Boolean,
+    round: Boolean,
+  },
+  computed: {
+    colorClasses() {
+      const color = this.color;
+      const baseClasses = `bg-${color}-600 text-${color}-100 border-${color}-600 hover:bg-${color}-700 hover:border-${color}-700 hover:text-white`;
+      const outlineClasses = `border-${color}-600 bg-white text-${color}-600 hover:bg-${color}-600 hover:border-${color}-600 hover:text-white`;
+      return this.outline ? outlineClasses : baseClasses;
+    },
+    sizeClasses() {
+      const sizeMappings = {
+        sm: "h-8 text-sm px-4",
+        md: "h-10 text-md  px-6",
+        lg: "h-12 text-lg px-12",
+      };
+
+      return sizeMappings[this.size] || sizeMappings.md;
+    },
+    btnClasses() {
+      const borderRadiusClasses = this.round ? "rounded-full" : "rounded";
+      return `${this.colorClasses} ${this.sizeClasses} ${borderRadiusClasses}`;
+    },
+    buttonType() {
+      if (this.href) {
+        return "a";
+      } else {
+        return "button";
+      }
+    },
+  },
+};
+</script>
--- src/components/DogCard.vue
+++ src/components/DogCard.vue
@@ -11,23 +11,28 @@
       />
     </figure>
 
-    <div class="w-full flex justify-end mt-4 grid-cols-1">
-      <button
+    <div class="w-full flex justify-end mt-4">
+      <Button
+        color="green"
+        customClass="w-full"
+        size="md"
+        outline="true"
         v-if="getActiveBreed"
-        class="btn"
         @click="this.$router.push(`/dog/view/${getActiveBreed}/${imgIndex}`)"
       >
-        View Details
-      </button>
+        <span class="capitalize">View Details</span>
+      </Button>
     </div>
   </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
+import Button from "./Button.vue";
 
 export default {
   name: "DogCard",
+  components: { Button },
   props: {
     imgURL: {
       type: String,
@@ -45,6 +50,23 @@ export default {
   },
   computed: {
     ...mapGetters("dogs", ["getActiveBreed"]),
+  },
+  data() {
+    return {
+      colors: [
+        "gray",
+        "yellow",
+        "orange",
+        "red",
+        "green",
+        "teal",
+        "blue",
+        "indigo",
+        "purple",
+        "pink",
+      ],
+      sizes: ["lg", "md", "sm"],
+    };
   },
 };
 </script>
